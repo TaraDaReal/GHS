f6b56cbcc01abcf657f79cc90481e49d0120c5a2
TaraDaReal SteamLT <112470727+TaraDaReal@users.noreply.github.com>
2024-11-28 17:33:33 +0200

    11/28
    Add movement relative to rotation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
     
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Camera cam;
+    public Vector2 mousePos;
 
     Vector2 velocity;
 
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,10 +25,27 @@ public class Player : MonoBehaviour
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
         velocity = Vector2.ClampMagnitude(velocity, 1f);
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
+        
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        moveCharacter(velocity);
+    }
+
+    void moveCharacter(Vector2 dir)
+    {
+        Quaternion rotation = Quaternion.Euler(0f, 0f, rb.rotation);
+
+        dir = rotation * dir;
+
+        rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
     }
 }
